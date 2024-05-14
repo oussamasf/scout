4493771ec03d8de370cc4fb80998607b14fd6a07
oussama seffai <ousf.dev@gmail.com>
2024-05-14 17:52:50 +0100

    feat(clap): init scout logger

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{arg, Command};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+
+pub fn run_cli() -> Command<'static> {
+    Command::new("Scout")
+        .about("Log analysis tool")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("analyze")
+                .about("Analyze log files")
+                .arg(arg!(-f --file <FILE> "Specifies the log file to analyze").required(true))
+                .arg(arg!(-t --type <TYPE> "Specifies the type of log file"))
+                .arg(arg!(-o --output <OUTPUT> "Specifies the output file for the report")),
+        )
+        .subcommand(
+            Command::new("view")
+                .about("View the contents of a log file")
+                .arg(arg!(-f --file <FILE> "Specifies the log file to view").required(true)),
+        )
+        .subcommand(
+            Command::new("filter")
+                .about("Filter log entries based on a query")
+                .arg(arg!(-f --file <FILE> "Specifies the log file to filter").required(true))
+                .arg(
+                    arg!(-q --query <QUERY> "Defines the query for filtering entries")
+                        .required(true),
+                ),
+        )
+        .subcommand(Command::new("help").about("Shows help information"))
+        .subcommand(Command::new("version").about("Shows version information"));
+}
+
+pub fn handle_matches(matches: &clap::ArgMatches) {
+    match matches.subcommand() {
+        Some(("analyze", sub_matches)) => {
+            let file_path = sub_matches.get_one::<String>("file").unwrap();
+            let log_type = sub_matches
+                .get_one::<String>("type")
+                .unwrap_or(&"default".to_string());
+            let output = sub_matches
+                .get_one::<String>("output")
+                .unwrap_or(&"report.txt".to_string());
+
+            println!(
+                "Analyzing {} as {} type. Results will be saved to {}",
+                file_path, log_type, output
+            );
+            // TODO
+        }
+        Some(("view", sub_matches)) => {
+            let file_path = sub_matches.get_one::<String>("file").unwrap();
+
+            if let Ok(file) = File::open(file_path) {
+                let reader = BufReader::new(file);
+                for line in reader.lines() {
+                    if let Ok(line) = line {
+                        println!("{}", line);
+                    }
+                }
+            } else {
+                println!("Failed to open file: {}", file_path);
+            }
+        }
+        Some(("filter", sub_matches)) => {
+            let file_path = sub_matches.get_one::<String>("file").unwrap();
+            let query = sub_matches.get_one::<String>("query").unwrap();
+
+            println!(
+                "Filtering entries from {} with query '{}'",
+                file_path, query
+            );
+            // TODO
+        }
+        Some(("help", _)) => {
+            app.print_help().unwrap();
+            println!();
+        }
+        Some(("version", _)) => {
+            println!("Scout version 1.0");
+        }
+        _ => unreachable!("Exhaustive subcommand matching in Clap"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,15 @@
-use std::path::PathBuf;
-
-use clap::{arg, command, value_parser, ArgAction, Command};
+use clap::{command, Arg, ArgAction};
 
 fn main() {
-    let matches = command!()
-        .arg(arg!([name] "this appears on help man"))
-        .arg(
-            arg!(
-                -c --config <FILE> "Sets a custom config file"
-            )
-            .required(false)
-            .value_parser(value_parser!(PathBuf)),
-        )
-        .arg(arg!(
-            -d --debug ... "Turn debugging information on"
-        ))
-        .subcommand(
-            Command::new("test")
-                .about("does testing things")
-                .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
-        )
+    let matches = command!() // requires `cargo` feature
+        .arg(Arg::new("name").action(ArgAction::Append))
         .get_matches();
 
-    if let Some(name) = matches.get_one::<String>("name") {
-        println!("Value for name: {name}");
-    }
+    let args = matches
+        .get_many::<String>("name")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
 
-    if let Some(config_path) = matches.get_one::<PathBuf>("config") {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    match matches
-        .get_one::<u8>("debug")
-        .expect("Count's are defaulted")
-    {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.get_flag("list") {
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
-        }
-    }
+    println!("names: {:?}", &args);
 }
